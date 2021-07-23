e982d8917461fcdcebabc236b578dbe49c0d09b8
ndeva3199 <ndeva3199@gmail.com>
2021-07-24 02:23:04 +0700

    cm

--- src/api/constance.js
+++ src/api/constance.js
@@ -1,8 +1,44 @@
 import Config from "react-native-config";
-
+import {accessToken} from '../config/config'
 
 export const withFetch = ({ url, method, body }) => {
   var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  var raw = body ? JSON.stringify(body) : undefined;
+  var requestOptions = {
+    method: method || "GET",
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
+
+  return fetch(Config.API_URL + url, requestOptions)
+    .then(response => response.json())
+    .then(result => {
+      if (result.resultCode === 1) return {
+        error: false,
+        data: result.data,
+        message: result.message
+      }
+      return {
+        error: true,
+        data: result.data,
+        message: result.message
+      }
+    })
+    .catch(error => {
+      console.log('error', error)
+      return {
+        error: true,
+        data: null,
+        message: "Please check your network"
+      }
+    });
+}
+
+export const withAuth = ({ url, method, body }) => {
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", "Bearer "+accessToken);
   myHeaders.append("Content-Type", "application/json");
   var raw = body ? JSON.stringify(body) : undefined;
   var requestOptions = {
--- src/config/config.js
+++ src/config/config.js
@@ -1,1 +1,2 @@
 export const API_URL = "https://lovezone-api.herokuapp.com/"
+export var accessToken = null
